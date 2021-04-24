e5a07ea518d7870ade5e9c4ee05aeb1bd98b5ef9
Kaveen LK <kmklaksitha@gmail.com>
2021-04-24 13:39:20 +0530

    nutrition management updates

---
+++ app/src/main/java/com/example/getfit/Nutrition/MealList.java
@@ -0,0 +1,16 @@
+package com.example.getfit.Nutrition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.getfit.R;
+
+public class MealList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_list);
+    }
+}
---
+++ app/src/main/java/com/example/getfit/Nutrition/NutritionNavigate.java
@@ -0,0 +1,16 @@
+package com.example.getfit.Nutrition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.getfit.R;
+
+public class NutritionNavigate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrition_navigate);
+    }
+}
