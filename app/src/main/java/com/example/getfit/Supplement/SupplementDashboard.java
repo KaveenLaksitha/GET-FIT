d24cab46f086a88864b9805bc9860f040052ab4e
NaliniSandunika <sandunikahindurangala92@gmail.com>
2021-04-25 19:12:18 +0530

    Supplement updated

---
+++ app/src/main/java/com/example/getfit/Supplement/SupplementDashboard.java
@@ -0,0 +1,16 @@
+package com.example.getfit.Supplement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.getfit.R;
+
+public class SupplementDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplement_dashboard);
+    }
+}
